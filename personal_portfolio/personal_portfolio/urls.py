2741e711176b2e1d283d409f80d9db08294f3b48
zahrakoohestani <zahrakoohestani11@gmail.com>
2020-06-24 16:21:47 +0430

    2nd

--- personal_portfolio/personal_portfolio/urls.py
+++ personal_portfolio/personal_portfolio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
+    path('', views.home),
 ]
